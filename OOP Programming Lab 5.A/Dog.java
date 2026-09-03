0b830d2bc8fc5c895fd68fbfca0eab61d8eeed1e
sabrinaanika2005-star <sabrinaanika2005@gmail.com>
2026-09-03 13:31:58 +0600

    Add files via upload

--- OOP Programming Lab 5.A/Dog.java
+++ OOP Programming Lab 5.A/Dog.java
@@ -1,1 +1,21 @@
+class Dog {
+    String name, breed;
 
+    Dog(String n, String b) {
+        name = n; breed = b;
+    }
+
+    void setName(String n) { name = n; }
+    void setBreed(String b) { breed = b; }
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog("Tommy", "Labrador");
+        Dog d2 = new Dog("Bruno", "Bulldog");
+
+        d1.setName("Max");
+        d2.setBreed("Poodle");
+
+        System.out.println(d1.name + " - " + d1.breed);
+        System.out.println(d2.name + " - " + d2.breed);
+    }
+}
--- OOP Programming Lab 5.A/Person.java
+++ OOP Programming Lab 5.A/Person.java
@@ -1,1 +1,20 @@
+class Person {
+    String name;
+    int age;
 
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Sabrina", 20);
+        Person person2 = new Person("Anika", 21);
+
+        System.out.println("Person 1 Name: " + person1.name);
+        System.out.println("Person 1 Age: " + person1.age);
+
+        System.out.println("Person 2 Name: " + person2.name);
+        System.out.println("Person 2 Age: " + person2.age);
+    }
+}
